23ca2c0336568641a562711a4ea96b99a7b584f8
zhangheng <hengzhang2@qq.com>
2018-04-26 16:55:11 +0800

    添加注释

--- src/main/java/com/zhangheng/common/BaseService.java
+++ src/main/java/com/zhangheng/common/BaseService.java
@@ -15,15 +15,45 @@ import com.github.pagehelper.PageInfo;
 @Service
 public interface BaseService<T extends BaseEntity> {
 
+	/**
+	 * 根据主键ID查询数据
+	 * @param id
+	 * @return
+	 */
 	public BaseEntity findById(Number id);
 
+	/**
+	 * 添加实体类
+	 * @param t
+	 */
 	public void add(T t);
 	
+	/**
+	 * 分页查询数据 返回一个分页对象（包含分页的导航页）
+	 * @param pageNum
+	 * @param pageSize
+	 * @param t 查询条件
+	 * @return
+	 */
 	public PageInfo<T> pageList(Integer pageNum, Integer pageSize, T t);
 
+	/**
+	 * 查询
+	 * @param t
+	 * @return
+	 */
 	public List<T> findList(T t);
 
+	/**
+	 * 根据主键删除
+	 * @param id
+	 */
 	public void deleteById(Number id);
 
+	/**
+	 * 根据主键更新
+	 * @param id
+	 * @param t
+	 */
 	public void update(Number id,T t);
 }
