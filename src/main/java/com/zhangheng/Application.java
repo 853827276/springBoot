1f1c9369e381e6e4352672b0cc56d65d5c3a4523
zhangheng <hengzhang2@qq.com>
2018-05-02 08:51:48 +0800

    设置banner 启动关闭打印

--- src/main/java/com/zhangheng/Application.java
+++ src/main/java/com/zhangheng/Application.java
@@ -1,11 +1,13 @@
 package com.zhangheng;
 
 import java.util.Properties;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 import com.github.pagehelper.PageHelper;
 
 /**
@@ -19,10 +21,21 @@ import com.github.pagehelper.PageHelper;
 @MapperScan("com.zhangheng.dao")
 public class Application {
 
+	/**
+	 * @author zhangh
+	 * @date 2018年4月28日下午5:18:38
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		//下面的代码可以自定义banner 还可以控制banner的显示方式（log console off）
+//		SpringApplication app = new SpringApplication(Application.class);
+//		app.setBannerMode(Banner.Mode.OFF);
+//		app.run(args);
 	}
-
+	
+	
+	
 	@Bean
 	public PageHelper pageHelper() {
 		PageHelper pageHelper = new PageHelper();
