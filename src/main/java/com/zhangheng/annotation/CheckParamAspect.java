63bc0556fc668af0c3c384e8767c43666791917c
zhangheng <hengzhang2@qq.com>
2018-05-02 18:16:45 +0800

    去掉之前的asject 的order的引用

--- src/main/java/com/zhangheng/annotation/CheckParamAspect.java
+++ src/main/java/com/zhangheng/annotation/CheckParamAspect.java
@@ -9,9 +9,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
 import com.zhangheng.common.ResultEnum;
 import com.zhangheng.exception.MyException;
 import com.zhangheng.util.AnnotationUtil;
@@ -24,7 +24,7 @@ import com.zhangheng.util.AnnotationUtil;
  */
 @Aspect
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(1)
 public class CheckParamAspect {
 
 	private static Logger logger = Logger.getLogger(CheckParamAspect.class);
